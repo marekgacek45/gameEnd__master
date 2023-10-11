9b926a9ec7120149a27abfd72d2518c8ae7639da
marekgacek45 <marekgacek45@gmail.com>
2023-10-11 19:26:28 +0200

    fix bug with modal

--- app/Http/Controllers/AdminTagsController.php
+++ app/Http/Controllers/AdminTagsController.php
@@ -8,29 +8,57 @@ use Illuminate\Http\Request;
 
 class AdminTagsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
 
         $tags = Tag::all();
         $categories = Category::all();
-       
-               return view('admin.tag.index',['tags'=>$tags,'categories'=>$categories]);
-           }
 
-           public function store()
-           {
-       
-               $attributes = request()->validate([
-                   'name' => 'required',
-                   'category_id' => ['required'],
-                  
-               ]);
-       
+        return view('admin.tag.index', ['tags' => $tags, 'categories' => $categories]);
+    }
+
+    public function store()
+    {
+
+        $attributes = request()->validate([
+            'name' => 'required',
+            'category_id' => ['required'],
+
+        ]);
+
+
+
+        $tag = Tag::create($attributes);
+
+
+        return redirect(route('admin.tag.index'))->with('success', 'Post został dodany');
+    }
+
+    public function update(Tag $tag){
+
+        // dd(request());
+                $attributes = request()->validate([
+                    'name' => 'required',
+                    'category_id' => 'required',
+                    
+                ]);
+                
+        
+                $tag->update($attributes);
+        
+        
+                return redirect(route('admin.tag.index'))->with('success', 'Kategoria została zaktualizowana');
+        
+            }
+            public function destroy(Tag $tag)
+            {
+                
+                
+            
                
-       
-               $tag = Tag::create($attributes);
-       
-       
-               return redirect(route('admin.tag.index'))->with('success', 'Post został dodany');
-}
+                $tag->delete();
+            
+                return back()->with('success', 'Tag została usunięta, a tagi mają teraz kategorię NULL');
+            }
 }
--- resources/views/admin/tag/index.blade.php
+++ resources/views/admin/tag/index.blade.php
@@ -23,7 +23,20 @@
                             
                             </th>
 
-                           
+                            <td class="px-6 py-4 flex">
+                                <form action="{{route('admin.tag.destroy',$tag->id)}}" method="Post">
+@csrf
+@method('DELETE')
+                                    <button type="submit" class="border px-8 py-4">{{$tag->id}}</button>
+                                </form>
+
+                                
+                                <button data-modal-target="modal-edit" data-modal-toggle="modal-edit"
+        class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button">
+        Edytuj
+    </button>
+                            </td>
          
                         <tr>
                     @endforeach
@@ -85,6 +98,47 @@
             </div>
         </div>
     </div>
-
-
+ 
+{{-- MODAL EDIT --}}
+<div id="modal-edit" tabindex="-1"
+class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div class="relative w-full max-w-lg max-h-full">
+    <!-- Modal content -->
+    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- Modal header -->
+        <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
+            <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                Edytuj Kategorię
+            </h3>
+            <button type="button"
+                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-hide="medium-modal">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+        </div>
+        <!-- Modal body -->
+        <div class="p-6 space-y-6">
+            <form action="{{ route('admin.tag.update',$tag->id) }}" method="post" id="updateTag">
+                @csrf
+                @method('patch')
+                <p>id={{$tag->id}}</p>
+                <x-form.input name="name" namePl="Nazwa Kategori" class=" w-full xl:w-3/4 "
+                    placeholder="Wprowadź tytuł" value="{{$tag->name}}"/>
+            </form>
+        </div>
+        <!-- Modal footer -->
+        <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+            <button form="updateTag" data-modal-hide="modal-edit" type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Zaktualizuj</button>
+            <button data-modal-hide="modal-edit" type="button"
+                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Anuluj</button>
+        </div>
+    </div>
+</div>
+</div>
 </x-layouts.admin>
