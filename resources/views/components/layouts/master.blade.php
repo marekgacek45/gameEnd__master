4968f110d1350ebaeedbca7d61f8e3e7410fc946
marekgacek45 <marekgacek45@gmail.com>
2023-10-12 06:52:27 +0200

    add polish language to app

--- resources/views/components/layouts/master.blade.php
+++ resources/views/components/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Gameend')</title>
-    @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     
 </head>
 <body class="bg-white text-black flex flex-col justify-between h-screen">
